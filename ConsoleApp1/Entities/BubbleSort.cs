b7e194d3cc2b7200b070fe6523186986da7cef0a
Paulo Souza <paulosouzx14@gmail.com>
2024-09-25 11:41:27 +0100

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Entities/BubbleSort.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1.Entities
+{
+    public class BubbleSort
+    {
+
+        public void MakeSort(int[] numeros)
+        {
+            int tamanho = numeros.Length;
+
+            for (int i = 0; i < tamanho - 1; i++)
+            {
+                for (int j = 0; j < tamanho - i - 1; j++)
+                {
+                    if (numeros[j] > numeros[j + 1])
+                    {
+                        int temp = numeros[j];
+                        numeros[j] = numeros[j + 1];
+                        numeros[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1.Entities;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 3, 5, 10, 0, 124, 69 };
+
+            BubbleSort bubbleSort = new BubbleSort();
+
+            bubbleSort.MakeSort(numeros);
+
+            Console.WriteLine(string.Join(" , ", numeros));
+        }
+    }
+}
